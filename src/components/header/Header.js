b5cccc508eef9a13f53c77b4dba1e41fa15fbea7
valendaquino <vdaquino@udesa.edu.ar>
2021-09-20 13:41:12 -0300

    carpetas

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from './node_modules/react';
 import Layers from '../Layers/Layers';
 import './Header.css';
 import FilterCard from '../FilterCard/FilterCard';
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import Card from '../Card/Card';
+import React, {Component} from './node_modules/react';
+import Card from '../card/Card';
 import './Main.css';
 
 class Main extends Component{
